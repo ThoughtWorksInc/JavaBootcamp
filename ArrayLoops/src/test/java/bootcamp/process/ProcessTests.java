0861ed21d9683acc5ab0e3e0cce64b5151dbd704
obashir68 <omar.bashir@thoughtworks.com>
2020-04-01 21:31:01 +0100

    Arrays and loops exercise

---
+++ ArrayLoops/src/test/java/bootcamp/process/ProcessTests.java
@@ -0,0 +1,27 @@
+package bootcamp.process;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ProcessTests {
+
+    @Test
+    public void testGetSummary() {
+        fail("Not Implemented");
+    }
+
+    @Test
+    public void testGetSummaryEmptyArray() {
+        fail("Not Implemented");
+    }
+
+    @Test
+    public void testGetValue() {
+        fail("Not Implemented");
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testGetValueOutOfArrayBounds() {
+        fail("Not Implemented");
+    }
+}
