cdc85a1efb208afe2408ea988b1af3bcdc3659a2
willensoll <william.ensoll@gmail.com>
2020-04-02 17:15:17 +0100

    calculator tests passing

--- Calculator/src/main/java/bootcamp/calculator/Calculator.java
+++ Calculator/src/main/java/bootcamp/calculator/Calculator.java
@@ -3,9 +3,29 @@ package bootcamp.calculator;
 import bootcamp.data.Params;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class Calculator {
+    BigDecimal result;
+
     public BigDecimal calculate(final Params params) {
-        return null;
+
+        switch (params.getOperator()) {
+            case "+":
+                result = params.getX().add(params.getY());
+                break;
+            case "-":
+                result = params.getX().subtract(params.getY());
+                break;
+            case "x":
+                result = params.getX().multiply(params.getY());
+                break;
+            case "/":
+                result = params.getX().divide(params.getY());
+                break;
+            default:
+                throw new IllegalArgumentException("Not a valid input");
+        }
+        return result.setScale(2, BigDecimal.ROUND_HALF_EVEN).stripTrailingZeros();
     }
 }
