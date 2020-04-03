5753d3732ec40094b83c2b379404e69b1329c37c
James Cotcher <james.cotcher@thoughtworks.com>
2020-04-03 14:16:40 +0100

    Kata 3 - array loops

--- ArrayLoops/src/main/java/bootcamp/data/Summary.java
+++ ArrayLoops/src/main/java/bootcamp/data/Summary.java
@@ -1,11 +1,57 @@
 package bootcamp.data;
 
+import java.util.Arrays;
+
+import static java.lang.Double.parseDouble;
+
 public class Summary {
-    private final double min = Double.NaN; //FIXME
-    private final double max = Double.NaN; //FIXME
-    private final double sum = Double.NaN; //FIXME
-    private final double count = Double.NaN; //FIXME
-    private final double average = Double.NaN; //FIXME
+
+    private final double min;
+    private final double max;
+    private final double sum;
+    private final double count;
+    private final double average;
+
+    public Summary(double[] array) {
+        this.min = findMin(array);
+        this.max = findMax(array);
+        this.sum = findSum(array);
+        this.count = findCount(array);
+        this.average = findAverage(array);
+
+    }
+
+    public double findMin(double[] array) {
+        if (array.length == 0) {
+            return Double.NaN;
+        }
+        return Arrays.stream(array).reduce((a, b) -> Math.min(a, b)).getAsDouble();
+    }
+
+    public double findMax(double[] array) {
+        if (array.length == 0) {
+            return Double.NaN;
+        }
+        return Arrays.stream(array).reduce((a, b) -> Math.max(a, b)).getAsDouble();
+    }
+
+    public double findSum(double[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        return Arrays.stream(array).reduce((a, b) -> a + b).getAsDouble();
+    }
+
+    public double findCount(double[] array) {
+        return array.length;
+    }
+
+    public double findAverage(double[] array) {
+        if (array.length == 0) {
+            return Double.NaN;
+        }
+        return this.sum/this.count;
+    }
 
     public double getMin() {
         return min;
