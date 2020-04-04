7cc8f04093b70b6203c7786e009000fe2385dfdb
Katherin Siracusa <katherin.siracusa@babylonhealth.com>
2020-04-04 18:42:24 +0100

    Implement and fix tests for Classes Kata

--- Classes/src/main/java/bootcamp/kata/process/Processor.java
+++ Classes/src/main/java/bootcamp/kata/process/Processor.java
@@ -6,8 +6,29 @@ package bootcamp.kata.process;
 import bootcamp.kata.data.Params;
 import bootcamp.kata.data.Results;
 
+import java.math.BigDecimal;
+
 public class Processor {
     public Results process(final Params params) {
-        return null; // FIXME
+        final BigDecimal x = params.getX();
+        final BigDecimal y = params.getY();
+
+        return new Results(sum(x, y), difference(x, y), product(x, y), quotient(x, y));
+    }
+
+    private BigDecimal sum(final BigDecimal x, final BigDecimal y) {
+        return x.add(y);
+    }
+
+    private BigDecimal difference(final BigDecimal x, final BigDecimal y) {
+        return x.subtract(y);
+    }
+
+    private BigDecimal product(final BigDecimal x, final BigDecimal y) {
+        return x.multiply(y);
+    }
+
+    private BigDecimal quotient(final BigDecimal x, final BigDecimal y) {
+        return x.divide(y);
     }
 }
