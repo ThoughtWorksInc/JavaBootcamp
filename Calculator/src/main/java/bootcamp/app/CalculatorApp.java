8d33d3e51bc6b10ae0040005d1d41c36b2e6de42
Katherin Siracusa <katherin.siracusa@babylonhealth.com>
2020-04-05 12:40:12 +0100

    Calculator kata, without cl

--- Calculator/src/main/java/bootcamp/app/CalculatorApp.java
+++ Calculator/src/main/java/bootcamp/app/CalculatorApp.java
@@ -1,7 +1,13 @@
 package bootcamp.app;
 
+import bootcamp.calculator.Calculator;
+import bootcamp.data.Params;
+
+import java.math.BigDecimal;
+
 public class CalculatorApp {
     public static void main(final String[] args) {
-        throw new RuntimeException("Not yet implemented");
+        Params params = new Params(new BigDecimal(args[0]), new BigDecimal(args[1]), args[2]);
+        System.out.print(new Calculator().calculate(params));
     }
 }
--- Calculator/src/main/java/bootcamp/calculator/Calculator.java
+++ Calculator/src/main/java/bootcamp/calculator/Calculator.java
@@ -3,9 +3,31 @@ package bootcamp.calculator;
 import bootcamp.data.Params;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Calculator {
     public BigDecimal calculate(final Params params) {
-        return null;
+        BigDecimal x = params.getX();
+        BigDecimal y = params.getY();
+        BigDecimal result;
+
+        switch(params.getOperator()) {
+            case "+":
+                result = x.add(y);
+                break;
+            case "-":
+                result = x.subtract(y);
+                break;
+            case "x":
+                result = x.multiply(y);
+                break;
+            case "/":
+                result = x.divide(y);
+                break;
+            default:
+                throw new IllegalArgumentException("Operator must be one of: +, -, x, /");
+        }
+        return result.setScale(1, RoundingMode.HALF_UP).stripTrailingZeros();
     }
 }
