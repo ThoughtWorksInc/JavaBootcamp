b23920efb1806e32470b8abde315e5c3209fd814
Gloria Laoye <gloria.laoye@thoughtworks.com>
2020-04-06 15:55:17 +0100

    [Gloria] Completed 2DArrays

--- TwoDArrays/src/main/java/bootcamp/process/PersonLocator.java
+++ TwoDArrays/src/main/java/bootcamp/process/PersonLocator.java
@@ -6,11 +6,25 @@ import bootcamp.data.Person;
 import java.util.Optional;
 
 public class PersonLocator {
-    private final Person[][] people = null; //FIXME
+    private final Person[][] people;
 
-    //TODO Constructor
+    public PersonLocator(Person[][] people){
+        this.people = people;
+    }
 
     public Optional<Location> find(final Person person) {
-        return null; //FIXME
+        Location location = null;
+
+        for(int column = 0; column<people.length; column++){
+            for(int row = 0; row<people[column].length; row++){
+                Person currentPerson = people[column][row];
+                if(person.equals(currentPerson)){
+                    location = new Location(row, column);
+                    break;
+                }
+            }
+        }
+        return Optional.ofNullable(location);
     }
+
 }
