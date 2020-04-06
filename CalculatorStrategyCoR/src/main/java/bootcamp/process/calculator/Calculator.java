cf4cad12a439ab9c230f76a42386cfcb8bc0ba9c
obashir68 <omar.bashir@thoughtworks.com>
2020-04-06 08:59:58 +0100

    Explanatory comment in Calculator

--- CalculatorStrategyCoR/src/main/java/bootcamp/process/calculator/Calculator.java
+++ CalculatorStrategyCoR/src/main/java/bootcamp/process/calculator/Calculator.java
@@ -7,6 +7,8 @@ import bootcamp.process.element.ElementFactory;
 public class Calculator {
     private final ElementFactory factory = null; //FIXME
 
+    //TODO Constructor to create ElementFactory Chain of Responsibility.
+
     public Result calculate(final Params params) {
         //FIXME using the factory and implementations of ProcessingElement
         return null;
