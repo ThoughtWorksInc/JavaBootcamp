1c2b566612eb7d5642837d7f153123f7ae48e336
katherin siracusa <katherin.siracusa@gmail.com>
2020-04-07 12:00:34 +0100

    Add guard clause to ensure 3 arguments

--- Calculator/src/main/java/bootcamp/app/CalculatorApp.java
+++ Calculator/src/main/java/bootcamp/app/CalculatorApp.java
@@ -7,6 +7,9 @@ import java.math.BigDecimal;
 
 public class CalculatorApp {
     public static void main(final String[] args) {
+        if (args.length != 3) {
+            System.out.print("Incorrect number of arguments given: Provide 2 numbers and one operator.");
+        }
         Params params = new Params(new BigDecimal(args[0]), new BigDecimal(args[1]), args[2]);
         System.out.print(new Calculator().calculate(params));
     }
