bd408d417a8ee8a071f53fb96f0bc9933de6deb9
obashir68 <omar.bashir@thoughtworks.com>
2020-04-07 10:57:17 +0100

    katas on collections

---
+++ PeopleAddresses/src/main/java/bootcamp/directory/AddressDirectory.java
@@ -0,0 +1,30 @@
+package bootcamp.directory;
+
+import bootcamp.data.Address;
+import bootcamp.data.Person;
+import bootcamp.data.PersonAddressPair;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class AddressDirectory {
+    private final Map<Person, Address> directory = null; //FIXME
+
+    public AddressDirectory(final List<PersonAddressPair> addressList) {
+        //TODO convert addressList to a HashMap assigned to directory.
+    }
+
+    public Optional<Address> getAddress(final Person person) {
+        //FIXME
+        return null;
+    }
+
+    public void updateAddress(final PersonAddressPair personAddress) {
+        //FIXME
+    }
+
+    public void remove(final Person person) {
+        //FIXME
+    }
+}
