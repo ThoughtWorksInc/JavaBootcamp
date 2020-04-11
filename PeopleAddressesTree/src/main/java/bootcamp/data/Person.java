bc0ba322f7de7a6f104389fa6e539eb5e5edc8a5
willensoll <william.ensoll@gmail.com>
2020-04-11 14:18:10 +0100

    peopleaddressestree kata

--- PeopleAddressesTree/src/main/java/bootcamp/data/Person.java
+++ PeopleAddressesTree/src/main/java/bootcamp/data/Person.java
@@ -1,11 +1,33 @@
 package bootcamp.data;
 
+import java.util.Objects;
+
 public class Person implements Comparable<Person> {
-    private final String firstName = null; //FIXME
-    private final String secondName = null; //FIXME
+    private final String firstName;
+    private final String secondName;
 
-    //TODO Constructor.
-    //TODO getters.
-    //TODO Fix the compilation error.
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, secondName);
+    }
 
+    public Person(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (firstName.equals(o.firstName) && secondName.equals(o.secondName)) {
+            return 0;
+        } else return -1;
+    }
 }
