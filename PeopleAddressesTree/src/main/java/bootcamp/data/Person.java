98c648b3fc67da3c538469ea877ea2740a22552d
James Cotcher <james.cotcher@thoughtworks.com>
2020-04-16 15:21:50 +0100

    AddressDirectory TreeMap Kata

--- PeopleAddressesTree/src/main/java/bootcamp/data/Person.java
+++ PeopleAddressesTree/src/main/java/bootcamp/data/Person.java
@@ -1,11 +1,40 @@
 package bootcamp.data;
 
+import java.util.Objects;
+
 public class Person implements Comparable<Person> {
-    private final String firstName = null; //FIXME
-    private final String secondName = null; //FIXME
+    private final String firstName;
+    private final String secondName;
 
-    //TODO Constructor.
-    //TODO getters.
-    //TODO Fix the compilation error.
+    public Person(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.firstName.equals(o.firstName) && this.secondName.equals(o.secondName)) {
+            return 0;
+        }
+        else return (this.toString().compareTo(o.toString()));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Person person = (Person) obj;
+        return (this.getFirstName() == person.getFirstName() && this.getSecondName() == person.getSecondName());
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.secondName;
+    }
 }
--- PeopleAddressesTree/src/main/java/bootcamp/directory/AddressDirectory.java
+++ PeopleAddressesTree/src/main/java/bootcamp/directory/AddressDirectory.java
@@ -7,24 +7,32 @@ import bootcamp.data.PersonAddressPair;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 
 public class AddressDirectory {
-    private final Map<Person, Address> directory = null; //FIXME
+    private final Map<Person, Address> directory;
 
     public AddressDirectory(final List<PersonAddressPair> addressList) {
-        //TODO convert addressList to a TreeMap assigned to directory.
+        TreeMap<Person, Address> newDir = new TreeMap<Person, Address>();
+        for(PersonAddressPair entry : addressList){
+            newDir.put(entry.getPerson(), entry.getAddress());
+        }
+        this.directory = newDir;
     }
 
     public Optional<Address> getAddress(final Person person) {
-        //FIXME
-        return null;
+        Address address = this.directory.get(person);
+        if (address == null) {
+            return Optional.empty();
+        }
+        return Optional.of(address);
     }
 
     public void updateAddress(final PersonAddressPair personAddress) {
-        //FIXME
+        this.directory.put(personAddress.getPerson(), personAddress.getAddress());
     }
 
     public void remove(final Person person) {
-        //FIXME
+        this.directory.remove(person);
     }
 }
