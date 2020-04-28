4c81aa6be76b8278819889f860b2c442812285b7
willensoll <william.ensoll@gmail.com>
2020-04-28 13:12:24 +0100

    finish histogram kata

--- WordHistogram/src/main/java/bootcamp/process/HistogramCalculator.java
+++ WordHistogram/src/main/java/bootcamp/process/HistogramCalculator.java
@@ -3,10 +3,11 @@ package bootcamp.process;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class HistogramCalculator {
     public Map<String, Integer> calculate(final List<String> words) {
-        HashMap<String, Integer> histogramCalc = new HashMap<>();
+        TreeMap<String, Integer> histogramCalc = new TreeMap<>();
         words.forEach(word -> {
             if (histogramCalc.containsKey(word)) {
                 histogramCalc.put(word, histogramCalc.get(word) + 1);
@@ -14,6 +15,7 @@ public class HistogramCalculator {
                 histogramCalc.put(word, 1);
             }
         });
+
         return histogramCalc;
     }
 }
