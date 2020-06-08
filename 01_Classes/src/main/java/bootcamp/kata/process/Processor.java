b94a911d89d1c949fe697faeb112a0187df2f7c6
Rahul Rakshit <rahul.rakshit@posteo.net>
2020-06-08 10:18:42 +0200

    Copy over 01_Classes kata

--- 01_Classes/src/main/java/bootcamp/kata/process/Processor.java
+++ 01_Classes/src/main/java/bootcamp/kata/process/Processor.java
@@ -3,11 +3,18 @@
  */
 package bootcamp.kata.process;
 
+import java.math.BigDecimal;
+
 import bootcamp.kata.data.Params;
 import bootcamp.kata.data.Results;
 
 public class Processor {
     public Results process(final Params params) {
-        return null; // FIXME
+        final BigDecimal sum = params.getX().add(params.getY());
+        final BigDecimal difference = params.getX().subtract(params.getY());
+        final BigDecimal product = params.getX().multiply(params.getY());
+        final BigDecimal quotient = params.getX().divide(params.getY());
+
+        return new Results(sum, difference, product, quotient);
     }
 }
