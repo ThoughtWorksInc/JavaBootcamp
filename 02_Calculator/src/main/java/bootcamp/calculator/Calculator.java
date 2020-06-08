6323d4267809bfba15d884017e8baca7ab5674d9
Stephanie Fashagba <stephanie.fashagba@thoughtworks.com>
2020-06-08 21:49:42 +0100

    Completed calculator with passing tests

--- 02_Calculator/src/main/java/bootcamp/calculator/Calculator.java
+++ 02_Calculator/src/main/java/bootcamp/calculator/Calculator.java
@@ -3,9 +3,41 @@ package bootcamp.calculator;
 import bootcamp.data.Params;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.function.BiFunction;
 
 public class Calculator {
+
+    private final HashMap<String, BiFunction<BigDecimal, BigDecimal, BigDecimal>> operatorList = getOperatorList();
+
     public BigDecimal calculate(final Params params) {
-        return null;
+
+        String operator = params.getOperator();
+        BigDecimal x = params.getX();
+        BigDecimal y = params.getY();
+
+        BiFunction<BigDecimal, BigDecimal, BigDecimal> getOperationFromList = operatorList.get(operator);
+
+        if (getOperationFromList == null) throw new IllegalArgumentException("Operator must be: '+', '-', '/' or 'x'");
+
+        return getOperationFromList
+                .apply(x, y)
+                .setScale(2, RoundingMode.HALF_UP)
+                .stripTrailingZeros();
     }
+
+    private HashMap<String, BiFunction<BigDecimal, BigDecimal, BigDecimal>> getOperatorList() {
+
+        HashMap<String, BiFunction<BigDecimal, BigDecimal, BigDecimal>> validOperatorList = new HashMap<>();
+
+        validOperatorList.put("+", BigDecimal::add);
+        validOperatorList.put("-", BigDecimal::subtract);
+        validOperatorList.put("x", BigDecimal::multiply);
+        validOperatorList.put("/", BigDecimal::divide);
+
+        return validOperatorList;
+
+    }
+
 }
