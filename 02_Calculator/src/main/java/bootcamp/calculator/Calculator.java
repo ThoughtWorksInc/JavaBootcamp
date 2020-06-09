57ef52bcfc3f909f73c63e4ad158f9488eeeb69e
Parth Aggarwal <parthvader89@gmail.com>
2020-06-09 19:42:26 +0100

    kata-2: now throws exceptions

--- 02_Calculator/src/main/java/bootcamp/calculator/Calculator.java
+++ 02_Calculator/src/main/java/bootcamp/calculator/Calculator.java
@@ -4,6 +4,7 @@ import bootcamp.data.Params;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Calculator {
     public BigDecimal calculate(final Params params) {
@@ -16,11 +17,19 @@ public class Calculator {
         if (operator.equals("-")) return scale(sub(x, y));
         if (operator.equals("x")) return scale(mult(x,y));
         if (operator.equals("/")) return scale(div(x,y));
-        else return null;
+
+        else throw new IllegalArgumentException("invalid operator");
     }
 
     private BigDecimal div(BigDecimal x, BigDecimal y) {
+        if (denominatorIsZero(y)){
+            throw new ArithmeticException("cannot divide by zero");
+        }
         return x.divide(y, MathContext.DECIMAL32);
+    }
+
+    private boolean denominatorIsZero(BigDecimal y) {
+        return y.setScale(2, RoundingMode.FLOOR).equals(new BigDecimal(0).setScale(2, RoundingMode.FLOOR));
     }
 
     private BigDecimal mult(BigDecimal x, BigDecimal y) {
