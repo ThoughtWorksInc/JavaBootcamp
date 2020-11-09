34724bdb082c087e2e28c8fbbec6acf449026d0c
obashir68 <obashir@yahoo.com>
2020-11-09 12:58:01 +0000

    Updated the ExceptionController

--- 17_CoursesService/src/main/java/bootcamp/controller/ExceptionController.java
+++ 17_CoursesService/src/main/java/bootcamp/controller/ExceptionController.java
@@ -6,7 +6,9 @@ import org.springframework.http.ResponseEntity;
 // TODO Controller Advice annotation
 public class ExceptionController /* TODO extend the  ResponseEntityExceptionHandler */{
     // TODO Exception handler annotation specifying NotFoundException class.
-    public ResponseEntity<String> handleNotFound(final NotFoundException exp) {
+    // TODO Create a class to encapsulate the error details and use that with ResponseEntity to return the following,
+    // TODO 1. Error message, 2. Timestamp
+    public ResponseEntity<?> handleNotFound(final NotFoundException exp) {
         return null; //FIXME
     }
 }
