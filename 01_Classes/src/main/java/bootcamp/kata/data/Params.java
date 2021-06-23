b4fdf3eeb26754135fb483de2a104d2ade14bff4
Laura Ghiorghisor <laura.ghiorghisor@gmail.com>
2021-06-23 19:19:58 +0100

    Fix constructors in Params and Results Add Processor functionality All tests passed

--- 01_Classes/src/main/java/bootcamp/kata/data/Params.java
+++ 01_Classes/src/main/java/bootcamp/kata/data/Params.java
@@ -6,17 +6,22 @@ package bootcamp.kata.data;
 import java.math.BigDecimal;
 
 public class Params {
-    private final BigDecimal x = new BigDecimal(0); // TODO use constructor initialisation
-    private final BigDecimal y = new BigDecimal(0); // TODO use constructor initialisation
+    private final BigDecimal x;
+    private final BigDecimal y;
 
+    public Params() {
+        this.x = new BigDecimal(0);
+        this.y = new BigDecimal(0);
+    }
     public Params(final BigDecimal x, final BigDecimal y) {
+        this.x = x;
+        this.y = y;
     }
 
     public final BigDecimal getX() {
-        return null; // FIXME
+        return x;
     }
 
-    public final BigDecimal getY() {
-        return null; // FIXME
+    public final BigDecimal getY() { return y;
     }
 }
--- 01_Classes/src/main/java/bootcamp/kata/process/Processor.java
+++ 01_Classes/src/main/java/bootcamp/kata/process/Processor.java
@@ -6,8 +6,14 @@ package bootcamp.kata.process;
 import bootcamp.kata.data.Params;
 import bootcamp.kata.data.Results;
 
+import java.math.BigDecimal;
+
 public class Processor {
     public Results process(final Params params) {
-        return null; // FIXME
+        BigDecimal sum =  params.getX().add(params.getY());
+        BigDecimal diff =  params.getX().subtract(params.getY());
+        BigDecimal prod = params.getX().multiply(params.getY());
+        BigDecimal quot = params.getX().divide(params.getY());
+        return new Results(sum, diff, prod, quot);
     }
 }
