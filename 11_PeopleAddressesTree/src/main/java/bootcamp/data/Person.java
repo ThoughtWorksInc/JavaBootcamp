bd8ac2cedbfa9411f36c9facd4345bb0fe50e5a2
Laura Ghiorghisor <laura.ghiorghisor@gmail.com>
2021-07-12 00:08:06 +0100

    Implement TreeMap
    Test passed

--- 11_PeopleAddressesTree/src/main/java/bootcamp/data/Person.java
+++ 11_PeopleAddressesTree/src/main/java/bootcamp/data/Person.java
@@ -1,11 +1,28 @@
 package bootcamp.data;
 
+import javax.management.ObjectName;
+import java.util.Comparator;
+
 public class Person implements Comparable<Person> {
-    private final String firstName = null; //FIXME
-    private final String secondName = null; //FIXME
+    private final String firstName;
+    private final String secondName;
 
-    //TODO Constructor.
-    //TODO getters.
-    //TODO Fix the compilation error.
+    // Fix the compilation error.
+    @Override
+    public int compareTo(Person person) {
+        return getFirstName().compareToIgnoreCase(person.getFirstName());
+    }
 
+    public Person(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
 }
