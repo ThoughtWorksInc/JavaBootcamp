255bdc1287b51b6ae7a90f9db8dfe221b9107f90
Laura Ghiorghisor <laura.ghiorghisor@gmail.com>
2021-07-12 00:35:21 +0100

    Implement Comparator

--- 12_PeopleAddressesTreeComparator/src/main/java/bootcamp/directory/AddressDirectory.java
+++ 12_PeopleAddressesTreeComparator/src/main/java/bootcamp/directory/AddressDirectory.java
@@ -11,21 +11,42 @@ public class AddressDirectory {
 
     public AddressDirectory(final List<PersonAddressPair> addressList) {
         directory = new TreeMap<>(new Comparator<Person>() {
-            //FIXME
+            @Override
+            public int compare(Person person, Person t1) {
+                return person.getFirstName().compareToIgnoreCase(t1.getFirstName());
+            }
         });
-        //TODO convert addressList to a TreeMap assigned to directory.
+        addressList.forEach((pair) -> directory.put(pair.getPerson(), pair.getAddress()));
+
     }
 
     public Optional<Address> getAddress(final Person person) {
-        //FIXME
-        return null;
+        return Optional.ofNullable(directory.get(person));
     }
 
     public void updateAddress(final PersonAddressPair personAddress) {
-        //FIXME
+        directory.replace(personAddress.getPerson(), personAddress.getAddress());
     }
 
     public void remove(final Person person) {
-        //FIXME
+        directory.remove(person);
+    }
+
+    public boolean hasPerson (Person person) {
+        if (directory.containsKey(person)) return true;
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AddressDirectory directory1 = (AddressDirectory) o;
+        return Objects.equals(directory, directory1.directory);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(directory);
     }
 }
