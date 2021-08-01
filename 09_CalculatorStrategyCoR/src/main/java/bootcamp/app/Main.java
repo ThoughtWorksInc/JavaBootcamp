08e4375467ced3df1a468f33b4d1b4baf0a2b3ec
Laura Ghiorghisor <laura.ghiorghisor@gmail.com>
2021-08-02 00:05:50 +0100

    package app

--- 09_CalculatorStrategyCoR/src/main/java/bootcamp/app/Main.java
+++ 09_CalculatorStrategyCoR/src/main/java/bootcamp/app/Main.java
@@ -6,10 +6,18 @@ import bootcamp.process.calculator.Calculator;
 import bootcamp.process.element.ElementFactory;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String [] args) {
-        Params params = new Params(new BigDecimal(4), new BigDecimal(5), "-");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number");
+        Integer n1 = scan.nextInt();
+        System.out.println("Enter another number");
+        Integer n2 = scan.nextInt();
+        System.out.println("Enter an opeation symbol - + x /");
+        String operation = scan.next();
+        Params params = new Params(new BigDecimal(n1), new BigDecimal(n2), operation);
         Calculator calculator = new Calculator();
         Result res = calculator.calculate(params);
         System.out.println(res);
